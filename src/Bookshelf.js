532d3634d73d75b722e8a65762657b7e44be2332
John Angel <j_angeldiaz@hotmail.com>
2018-09-03 18:58:19 -0500

    Display books passed as props

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -3,11 +3,36 @@ import React, {Component} from 'react'
 class Bookshelf extends Component {
 
     render(){
+        console.log('Books in bookshelf component', this.props.books);
         return(
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{this.props.title}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
+                        {
+                            this.props.books.map(book => (
+                            <li>
+                                <div className="book">
+                                <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.previewLink})`}}></div>
+                                    <div className="book-shelf-changer">
+                                        <select>
+                                            <option value="move" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.authors[0]}</div>
+                            </div>
+                                
+                            </li>
+
+
+                        ))}
                         <li>
                             <div className="book">
                                 <div className="book-top">
