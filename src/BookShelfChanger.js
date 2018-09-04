6d77e68dcfda31b69589943ef0591ffe3f3e535f
John Angel <j_angeldiaz@hotmail.com>
2018-09-04 18:51:14 -0500

    Current shelf set as default in dropdown list

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -5,9 +5,9 @@ class BookShelfChanger extends Component {
     render(){
         return(
             <div className="book-shelf-changer">
-                <select>
+                <select defaultValue={this.props.shelf}>
                     <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="currentlyReading">Currently Reading</option>                                                              
                     <option value="wantToRead">Want to Read</option>
                     <option value="read">Read</option>
                     <option value="none">None</option>
