e25434227d28ee110d20b53d345bccdcbcd8c475
John Angel <j_angeldiaz@hotmail.com>
2018-09-03 19:21:58 -0500

    Book component created - Display books by shelf

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
+import Book from './Book'
 
 class Bookshelf extends Component {
 
     render(){
-        console.log('Books in bookshelf component', this.props.books);
+        console.log('Books in', this.props.title, 'bookshelf: ', this.props.books);
         return(
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{this.props.title}</h2>
@@ -11,64 +12,9 @@ class Bookshelf extends Component {
                     <ol className="books-grid">
                         {
                             this.props.books.map(book => (
-                            <li>
-                                <div className="book">
-                                <div className="book-top">
-                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.previewLink})`}}></div>
-                                    <div className="book-shelf-changer">
-                                        <select>
-                                            <option value="move" disabled>Move to...</option>
-                                            <option value="currentlyReading">Currently Reading</option>
-                                            <option value="wantToRead">Want to Read</option>
-                                            <option value="read">Read</option>
-                                            <option value="none">None</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-authors">{book.authors[0]}</div>
-                            </div>
-                                
-                            </li>
-
-
-                        ))}
-                        <li>
-                            <div className="book">
-                                <div className="book-top">
-                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                                    <div className="book-shelf-changer">
-                                        <select>
-                                            <option value="move" disabled>Move to...</option>
-                                            <option value="currentlyReading">Currently Reading</option>
-                                            <option value="wantToRead">Want to Read</option>
-                                            <option value="read">Read</option>
-                                            <option value="none">None</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div className="book-title">To Kill a Mockingbird</div>
-                                <div className="book-authors">Harper Lee</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="book">
-                                <div className="book-top">
-                                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                                    <div className="book-shelf-changer">
-                                        <select>
-                                            <option value="move" disabled>Move to...</option>
-                                            <option value="currentlyReading">Currently Reading</option>
-                                            <option value="wantToRead">Want to Read</option>
-                                            <option value="read">Read</option>
-                                            <option value="none">None</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div className="book-title">Ender's Game</div>
-                                <div className="book-authors">Orson Scott Card</div>
-                            </div>
-                        </li>
+                                <Book book={book} key={book.id}></Book>
+                            ))
+                        }
                     </ol>
                 </div>
             </div>
