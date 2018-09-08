c432cf38701a22bed4e5660450e995a3fc0491bb
John Angel <j_angeldiaz@hotmail.com>
2018-09-07 19:36:50 -0500

    Display all the authors that a Book may contain

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,7 @@ class Book extends Component {
 
     onShelfSelected = (shelf) => (this.props.shelfSelected(shelf,this.props.book))
     render(){
-        //console.log('Rendering book ', this.props.book.title, ' from shelf' , this.props.book.shelf ? this.props.book.shelf : 'None');
+
         return(
             <li>
                 <div className="book">
@@ -14,8 +14,11 @@ class Book extends Component {
                         <BookShelfChanger shelf={this.props.book.shelf} shelfSelected={this.onShelfSelected}></BookShelfChanger>
                     </div>
                     <div className="book-title">{this.props.book.title}</div>
-                    {/*TODO: Iterate over the authors array*/}
-                    <div className="book-authors">{this.props.book.authors? this.props.book.authors[0]: ""}</div>
+                    <div className="book-authors">{
+                        /*If this.props.book.authors is defined it may contain more than 1 author */
+                        this.props.book.authors ? this.props.book.authors.reduce((accum, author, currIndex,array) => {return currIndex < array.length - 1 ? accum + author + " - ": accum + author},"") : ""
+                        }
+                    </div>
                 </div>
             </li>
         )
